96a654e5d5e0003b4119d025beab2d6b7b6fd89b
CJMoye <Cjmoye30@gmail.com>
2023-02-25 12:52:12 -0500

    Uploaded text into local storage and still figuring out how to correctly pull that data back from local storage. Very stuck

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,28 @@
+// Array has to be initialized outside of the load function - otherwise it will always be reset to empty
+
+// Create a function which goes through all of the IDs, and then pastes the text of the corresponding ID from the local storage
+function renderText (){
+  
+  $(".time-block").each(function(){
+    var blockIDs = $(this).attr("id");
+    console.log(blockIDs);
+  })
+}
+
+// var storedItemsLength = JSON.parse(localStorage.getItem("userBlockInput")).length;
+// console.log(JSON.parse(localStorage.getItem("userBlockInput"))[0]);
+// console.log(storedItemsLength);
+
+  // for (var i = 0; i < storedItemsLength; i++) {
+  //   // console.log(JSON.parse(localStorage.getItem("userBlockInput"))[i]);
+  //   myarray.push(JSON.parse(localStorage.getItem("userBlockInput"))[i]);
+  //   console.log(myarray[i].message);
+  // }
+
+
 $(function () {
+
   // TODO: Add code to display the current date in the header of the page.
-  // I feel this needs to be first so that I can style the color of the tabs for testing
   var currentDate = dayjs().format("dddd DD MMM, YYYY");
   $("#currentDay").text(currentDate);
 
@@ -8,31 +30,25 @@ $(function () {
   var currentHour = dayjs().format("HH");
   // console.log(currentHour);
 
-  var textBoxes = $(".description");
+  // Click event in place for all of the buttons
+  $(".saveBtn").click(function (e) {
+    var myarray = [];
+    // Traversing through the DOM to get to the parent element 
+    var userTextAreaInput = $(this).parent().children("textarea").val();
+    var boxId = $(this).parent().attr('id');
+    
+    // Creating an object for the input in the area the text is in
+    var input = {
+      boxId: boxId,
+      message: userTextAreaInput
+    }
+    
+    localStorage.setItem("userBlockInput", JSON.stringify(input));
+    userTextAreaInput = JSON.parse(localStorage.getItem("userBlockInput"));
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+    console.log(input);
 
-  var saveButtons = $(".saveBtn");
-  
-  saveButtons.click(function (e) {
-    console.log("Save button clicked");
-    var container = $(this).parent().children("textarea").val();
-    console.log(container);
   })
-
-  // On the save button click, then we need to run a function which then saves the input in the textarea to local storage
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
 
   // Function which pulls all of the class names called "Time-block", and for each of them which is basically a jquery for loop, we are then grabbing their ID
   $(".time-block").each(function () {
@@ -40,13 +56,10 @@ $(function () {
 
     // Conditional testing within the each/for loop which pulls the ID of each time block, and then checks that against the current hour
     if (calendarHour === currentHour) {
-      console.log(calendarHour + " is equal to " + currentHour);
       $(this).addClass("present");
     } else if (calendarHour < currentHour) {
-      console.log(calendarHour + " is less than " + currentHour);
       $(this).addClass("past");
     } else {
-      console.log(calendarHour + " is greater than " + currentHour);
       $(this).addClass("future");
     }
   })
@@ -56,3 +69,5 @@ $(function () {
   // attribute of each time-block be used to do this?
 
 });
+
+
