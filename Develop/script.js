4504ec9c8cb16b11f1773344b4b000fd9c05f82f
CJMoye <Cjmoye30@gmail.com>
2023-02-26 10:43:18 -0500

    Figured out the ID can be used as the local storage key which allows everything to be placed in local storage. No arrays or anything additional needed. Project completed!

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Array has to be initialized outside of the load function - otherwise it will always be reset to empty
 
 // Create a function which goes through all of the IDs, and then pastes the text of the corresponding ID from the local storage
-function renderText (){
-  
-  $(".time-block").each(function(){
+function renderText() {
+
+  $(".time-block").each(function () {
     var blockIDs = $(this).attr("id");
     console.log(blockIDs);
   })
@@ -13,11 +13,11 @@ function renderText (){
 // console.log(JSON.parse(localStorage.getItem("userBlockInput"))[0]);
 // console.log(storedItemsLength);
 
-  // for (var i = 0; i < storedItemsLength; i++) {
-  //   // console.log(JSON.parse(localStorage.getItem("userBlockInput"))[i]);
-  //   myarray.push(JSON.parse(localStorage.getItem("userBlockInput"))[i]);
-  //   console.log(myarray[i].message);
-  // }
+// for (var i = 0; i < storedItemsLength; i++) {
+//   // console.log(JSON.parse(localStorage.getItem("userBlockInput"))[i]);
+//   myarray.push(JSON.parse(localStorage.getItem("userBlockInput"))[i]);
+//   console.log(myarray[i].message);
+// }
 
 
 $(function () {
@@ -32,27 +32,20 @@ $(function () {
 
   // Click event in place for all of the buttons
   $(".saveBtn").click(function (e) {
-    var myarray = [];
     // Traversing through the DOM to get to the parent element 
     var userTextAreaInput = $(this).parent().children("textarea").val();
     var boxId = $(this).parent().attr('id');
-    
-    // Creating an object for the input in the area the text is in
-    var input = {
-      boxId: boxId,
-      message: userTextAreaInput
-    }
-    
-    localStorage.setItem("userBlockInput", JSON.stringify(input));
-    userTextAreaInput = JSON.parse(localStorage.getItem("userBlockInput"));
 
-    console.log(input);
-
+    // Figured out the first parameter can be something dynamic like the ID which will change each time depending on which save button is clicked which makes a lot more sense. Now, each button will have it's ID saved along with the input
+    localStorage.setItem(boxId, userTextAreaInput);
   })
 
   // Function which pulls all of the class names called "Time-block", and for each of them which is basically a jquery for loop, we are then grabbing their ID
   $(".time-block").each(function () {
     var calendarHour = $(this).attr('id');
+
+    var textArea = $(this).children("textarea").text(localStorage.getItem(calendarHour));
+    console.log($(this).children("textarea").val());
 
     // Conditional testing within the each/for loop which pulls the ID of each time block, and then checks that against the current hour
     if (calendarHour === currentHour) {
@@ -63,11 +56,8 @@ $(function () {
       $(this).addClass("future");
     }
   })
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
 });
+
+
 
 
