aebc706c918bf15ffdbb7bea7db79ebbcfdbae60
CJMoye <Cjmoye30@gmail.com>
2023-02-27 14:28:35 -0500

    Reorganized files to delete out Develop folder which is not needed. Add all info for README

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-// Array has to be initialized outside of the load function - otherwise it will always be reset to empty
-
-// Create a function which goes through all of the IDs, and then pastes the text of the corresponding ID from the local storage
-function renderText() {
-
-  $(".time-block").each(function () {
-    var blockIDs = $(this).attr("id");
-    console.log(blockIDs);
-  })
-}
-
-// var storedItemsLength = JSON.parse(localStorage.getItem("userBlockInput")).length;
-// console.log(JSON.parse(localStorage.getItem("userBlockInput"))[0]);
-// console.log(storedItemsLength);
-
-// for (var i = 0; i < storedItemsLength; i++) {
-//   // console.log(JSON.parse(localStorage.getItem("userBlockInput"))[i]);
-//   myarray.push(JSON.parse(localStorage.getItem("userBlockInput"))[i]);
-//   console.log(myarray[i].message);
-// }
-
-
 $(function () {
 
-  // TODO: Add code to display the current date in the header of the page.
+  // Code to display the current date in the header of the page.
   var currentDate = dayjs().format("dddd DD MMM, YYYY");
   $("#currentDay").text(currentDate);
 
   // Current Hour for calculations to style the divs
   var currentHour = dayjs().format("HH");
-  // console.log(currentHour);
 
   // Click event in place for all of the buttons
   $(".saveBtn").click(function (e) {
@@ -45,7 +22,6 @@ $(function () {
     var calendarHour = $(this).attr('id');
 
     var textArea = $(this).children("textarea").text(localStorage.getItem(calendarHour));
-    console.log($(this).children("textarea").val());
 
     // Conditional testing within the each/for loop which pulls the ID of each time block, and then checks that against the current hour
     if (calendarHour === currentHour) {
